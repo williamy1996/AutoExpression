4bdae3e2feb50b7ef18f5d1c55063e10519cfe32
williamy1996 <1581025137@qq.com>
2020-06-13 09:54:47 +0800

    flask_api on mac

--- examples/cls_exp.py
+++ examples/cls_exp.py
@@ -14,8 +14,7 @@ from solnml.estimators import Classifier
 parser = argparse.ArgumentParser()
 parser.add_argument('--time_limit', type=int, default=1200)
 parser.add_argument('--eval_type', type=str, default='holdout', choices=['holdout', 'cv', 'partial'])
-parser.add_argument('--ens_method', default='ensemble_selection',
-                    choices=['none', 'bagging', 'blending', 'stacking', 'ensemble_selection'])
+parser.add_argument('--ens_method', default='ensemble_selection',choices=['none', 'bagging', 'blending', 'stacking', 'ensemble_selection'])
 parser.add_argument('--n_jobs', type=int, default=1)
 
 args = parser.parse_args()
@@ -40,12 +39,7 @@ dm = DataManager(X_train, y_train)
 train_data = dm.get_data_node(X_train, y_train)
 test_data = dm.get_data_node(X_test, y_test)
 
-clf = Classifier(time_limit=time_limit,
-                 output_dir=save_dir,
-                 ensemble_method=ensemble_method,
-                 evaluation=eval_type,
-                 metric='acc',
-                 n_jobs=n_jobs)
+clf = Classifier(time_limit=time_limit,output_dir=save_dir,ensemble_method=ensemble_method,evaluation=eval_type,metric='acc',n_jobs=n_jobs)
 clf.fit(train_data)
 pred = clf.predict(test_data)
 print(balanced_accuracy_score(test_data.data[1], pred))
--- flask_api/test_api.py
+++ flask_api/test_api.py
@@ -10,10 +10,8 @@ import requests
 train_url = 'http://0.0.0.0:3724/train_pipeline'
 #test_url = 'http://0.0.0.0:3724/serve_prediction'
 model_url = 'http://0.0.0.0:3724/Model_information'
-train_files = {'data_file_X': open('./data/clyq_X.csv','rb'),
-	'data_file_y': open('./data/clyq_y.csv','rb'),
-	'params_file': open('./params/clf_params.yaml')}
-r_train = requests.post(train_url, data={'model_name':'testclf1','objective':'clf'},files=train_files)
+train_files = {'data_file_X': open('./data/clyq_X.csv','rb'),'data_file_y': open('./data/clyq_y.csv','rb'),'params_file': open('./params/clf_params.yaml')}
+r_train = requests.post(train_url, data={'model_name':'testclf2','objective':'clf'},files=train_files)
 #test_files = {'data_file_X': open('./data/clyq_X.csv','rb'),'data_file_y': open('./data/clyq_y.csv','rb')}
 #r_test = requests.post(test_url, data={'model_name':'testclf1','need_proba':True},files=train_files)
-r_model = requests.post(model_url, data={'model_name':'testclf1'})
+r_model = requests.post(model_url, data={'model_name':'testclf2'})
